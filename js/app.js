4760f6aeb64ae0e525ea268ad011234fd81f0391
ckgtwdev <>
2018-03-13 10:32:07 +0000

    Cheryl's Initial Commit

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@
  * Create a list that holds all of your cards
  */
 
-
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -11,20 +10,186 @@
  */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
+const parentElement = document.getElementById("parent-list");
+// const movesElement = document.getElementsByClassName("moves");
+const movesElement = document.getElementById("moves");
+const starOne = document.getElementById("star1");
+const starTwo = document.getElementById("star2");
+const starThree = document.getElementById("star3");
+let clockActive = false;
+let listChildren = parentElement.childNodes;
+let numClicks = 0;
+let faIcon = "";
+let matchingCards = [];
+let compareArray = [];
+let iconArray = new Array(
+  "fa fa-binoculars",
+  "fa fa-gem",
+  "fa fa-anchor",
+  "fa fa-bolt",
+  "fa fa-cube",
+  "fa fa-anchor",
+  "fa fa-leaf",
+  "fa fa-bicycle",
+  "fa fa-binoculars",
+  "fa fa-bomb",
+  "fa fa-leaf",
+  "fa fa-bomb",
+  "fa fa-bolt",
+  "fa fa-bicycle",
+  "fa fa-gem",
+  "fa fa-cube"
+);
+
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
 
+function displayCard(e) {
+  faIcon = e.target.childNodes[1].classList[1];
+  // show your clicked cards
+  e.target.classList.toggle("open");
+  e.target.classList.toggle("show");
+
+  // save font awesome icon for comparison and current clicked card id
+  compareArray.push(faIcon);
+  matchingCards.push(e.target.id);
+  if (compareArray.length == 2) {
+    for (let i = 0; i < matchingCards.length; i++) {
+      let cardx = document.getElementById(matchingCards[i]);
+      if (compareArray[0] === compareArray[1]) {
+        cardx.classList.toggle("match");
+        changeState();
+      } else {
+        // hide the cards
+        cardx.classList.toggle("open");
+        cardx.classList.toggle("show");
+      }
+    }
+    cleanArray();
+  }
+  updateCount(numClicks);
+}
+
+function updateCount(numClicks) {
+  movesElement.innerHTML = numClicks;
+}
+
+function updateStar(star, forceShow) {
+  if (forceShow === "block") {
+    star.style.display = "block";
+  } else {
+    star.style.display = "none";
+  }
+}
+
+function cleanArray() {
+  while (compareArray.length > 0) {
+    compareArray.pop();
+  }
+  while (matchingCards.length > 0) {
+    matchingCards.pop();
+  }
+}
+
+function startTimer() {
+  if (clockActive) {
+    let timer = document.getElementById("game-timer").innerHTML;
+    let timeArray = timer.split(":");
+    let hour = timeArray[0];
+    let min = timeArray[1];
+    let sec = timeArray[2];
+    if (sec == 59) {
+      if (min == 59) {
+        hour++;
+        min = 0;
+        if (hour < 10) hour = "0" + hour;
+      } else {
+        min++;
+      }
+      if (min < 10) min = "0" + min;
+      sec = 0;
+    } else {
+      sec++;
+      if (sec < 10) sec = "0" + sec;
+    }
+    // update our html timer
+    document.getElementById("game-timer").innerHTML =
+      hour + ":" + min + ":" + sec;
+    setTimeout(startTimer, 1000); // keep repeating with speed of 1 second
+  }
+}
+
+function changeState() {
+  if (clockActive == false) {
+    clockActive = true;
+    startTimer();
+  } else {
+    //check if all cards are flagged as matched
+    let matchList = document.getElementsByClassName("match");
+    if (matchList.length == 16) clockActive = false;
+  }
+}
+
+// randomise the layout
+shuffle(iconArray);
+
+// Get the element, add a click listener...
+parentElement.addEventListener("click", function(e) {
+  // e.target is the clicked element!
+  // If it was a list item
+  if (e.target && e.target.nodeName == "LI") {
+    // start timer
+    changeState();
+    // Display card symbol and increment click count.
+    numClicks++;
+    if (numClicks > 8) {
+      if (numClicks >= 9 && numClicks < 16) {
+        updateStar(star3, "none");
+      } else {
+        updateStar(star2, "none");
+      }
+    }
+
+    displayCard(e);
+  }
+});
+
+document.getElementById("restart").addEventListener("click", function() {
+  document.getElementById("game-timer").innerHTML = "00:00:00";
+  numClicks = 0;
+  updateCount(numClicks);
+  updateStar(star1, "block");
+  updateStar(star2, "block");
+  updateStar(star3, "block");
+  cleanArray();
+  shuffle(iconArray);
+
+  let j = 0;
+  for (let i = 0; i < listChildren.length; i++) {
+    if (listChildren[i].nodeName === "LI") {
+      var iconElement = document.createElement("i");
+      iconElement.setAttribute("class", iconArray[j]);
+      //hide the front of the card
+      listChildren[i].classList = "clearfix card";
+      //replace the previous Font Awesome icon with the reshuffled icon
+      listChildren[i].replaceChild(iconElement, listChildren[i].childNodes[1]);
+      j++;
+    }
+  }
+});
 
 /*
  * set up the event listener for a card. If a card is clicked:
